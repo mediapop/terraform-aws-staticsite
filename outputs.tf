40c238cb705d30bbcb1fbc059d5c6b8ba2616346
Kit Sunde <kitsunde@gmail.com>
2018-04-02 18:22:27 +0800

    Output the hosted zone id.

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "website_endpoint" {
   description = "The DNS name to point CNAME records to."
   value       = "${aws_s3_bucket.default.website_endpoint}"
 }
+
+output "hosted_zone_id" {
+  description = "The zone_id for the s3 bucket."
+  value       = "${aws_s3_bucket.default.hosted_zone_id}"
+}
