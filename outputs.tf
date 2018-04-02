a4c8241d5a6495cf24055dcbc04ca970b527d2eb
Kit Sunde <kitsunde@gmail.com>
2018-04-02 21:16:35 +0800

    Output the website domain

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "hosted_zone_id" {
   description = "The zone_id for the s3 bucket."
   value       = "${aws_s3_bucket.default.hosted_zone_id}"
 }
+
+output "website_domain" {
+  description = "The website_domain for the s3 bucket."
+  value       = "${aws_s3_bucket.default.website_domain}"
+}
